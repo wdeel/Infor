c9dbf643d29e31628b4a82bffa1fed72ff6d5f7f
wdlewis <wdlewis@clemson.edu>
2021-01-28 20:41:59 -0500

    query updated

--- Query.sql
+++ Query.sql
@@ -1,38 +1,6 @@
-SELECT *
-FROM BASE_PRICE;
-
-SELECT *
-FROM TOPPINGS;
-
-SELECT *
-FROM HAS_TOPPINGS;
-
-SELECT *
-FROM PIZZA;
-
-SELECT *
-FROM PIZZA_DISCOUNTS;
-
-SELECT *
-FROM DISCOUNTS;
-
-SELECT *
-FROM ORDER_DISCOUNTS;
-
-SELECT *
-FROM CUSTOMER;
-
-SELECT *
-FROM ORDERS;
-
-SELECT *
-FROM DINE_IN;
-
-SELECT *
-FROM SEAT_NUMBER;
-
-SELECT *
-FROM DELIVERY;
-
-SELECT *
-FROM PICK_UP;
+SELECT Event1.EventId, Event2.EventId 
+    FROM Events as Event1 join Events as Event2 ON Event1.ResourceID = Event2.ResourceID 
+    WHERE 
+        (Event1.StartedAt > Event2.StartedAt and Event2.StartedAt < Event1.EndedAt)
+        or 
+        (Event1.StartedAt > Event2.StartedAt and Event1.StartedAt < Event2.EndedAt);
